8f79cda8df9c22e8f815e09c665fdbdeb1ed1311
uzi0 <46659600+uzi0@users.noreply.github.com>
2020-04-28 22:19:28 -0400

    toggle all functionality

--- background.js
+++ background.js
@@ -1,7 +1,36 @@
+let muted = false;
+
+const toggleCurrent = () => {
+    chrome.tabs.query({currentWindow: true, active: true}, tabs => {
+        chrome.tabs.update(tabs[0].id, {muted: !tabs[0].mutedInfo.muted});
+    });
+}
+
+const toggleAll = () => {
+    muted = !muted;
+    // "populate" each window object with a list of tabs as a property
+    chrome.windows.getAll({populate: true}, windows => {
+        windows.forEach(({tabs}) => {
+            tabs.forEach(tab => {
+                chrome.tabs.update(tab.id, {muted});
+            });
+        });
+    });
+}
+
 chrome.commands.onCommand.addListener(command => {
     if (command === "mute_tab") {
-        chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-            chrome.tabs.update(tabs[0].id, {muted: !tabs[0].mutedInfo.muted});
-        });
+        toggleCurrent();
     };
+
+    if (command === "mute_all") {
+        toggleAll();
+    };
+
+    if (command === "mute_except") {
+        muted = false;
+        toggleAll()
+        // wait for toggleAll to finish executing
+        setTimeout(() => toggleCurrent(), 0);
+    }
 });
